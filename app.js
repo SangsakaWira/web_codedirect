0f0cbffe0948e812c2e8678d8384a71ea2438aa1
Sangsaka Wira <sangsakawira@gmail.com>
2018-12-30 10:34:25 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+var port = process.env.port || 3000;
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname+"/views/partials")
+
+app.get('/',function(req,res){
+    res.render("index");
+})
+
+app.get('/daftar',function(req,res){
+    res.render("daftar");
+})
+
+app.get('/fasilitas',function(req,res){
+    res.render("fasilitas");
+})
+
+app.get('/kelas',function(req,res){
+    res.render("kelas");
+})
+
+app.get('/projects',function(req,res){
+    res.render("projects");
+})
+
+app.get('/lokasi',function(req,res){
+    res.render("lokasi");
+})
+
+app.get('/404',function(req,res){
+    res.render("404");
+})
+
+app.listen(port,function(){
+    console.log("Server is starting at port "+port);
+})
