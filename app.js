f97dd87e479c8ce4e4c1c7b92cd7baa2807203f4
Sangsaka Wira <sangsakawira@gmail.com>
2018-12-30 10:45:29 +0700

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname+"/views/partials")
